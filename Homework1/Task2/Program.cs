f6ddd5f57453ac1d00f2d9065933b85934110d0a
Denis Malikov <malikovdy47@gmail.com>
2023-08-01 00:09:31 +0300

    Первая неудачная попытка решить задачу

--- Homework1/Task2/Program.cs
+++ Homework1/Task2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое 
+// число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Enter num1 :  ");
+
+num1 = Console.ReadLine();
+
+Console.WriteLine("Enter num2 :  ");
+
+num2 = Console.ReadLine();
+
+int max = strNum1;
+
+if (num1 > num2) max = num1;
+
+if (num1 < num2) max = num2;
+
+Console.WriteLine("max");
