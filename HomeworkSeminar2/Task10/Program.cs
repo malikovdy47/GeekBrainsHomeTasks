eb1a069c62e4b1a01a69fd7927aba664983ee36d
Denis Malikov <malikovdy47@gmail.com>
2023-08-03 23:11:07 +0300

    Решена задача №10 семинара2

--- HomeworkSeminar2/Task10/Program.cs
+++ HomeworkSeminar2/Task10/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Enter num : ");
+string strNum = Console.ReadLine();
+
+int num = int.Parse(strNum);
+
+if (num > 99&&num < 1000)
+    {
+        int num2 = num % 100 / 10;
+        Console.WriteLine($"{num2}");
+    }
+    else
+    {
+        Console.WriteLine("Введенное число не является трехзначным");
+    }
