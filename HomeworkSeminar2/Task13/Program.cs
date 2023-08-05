b394a8a35cc5c37459283cf457095eca7c276ee7
Denis Malikov <malikovdy47@gmail.com>
2023-08-05 16:51:33 +0300

    Решена задача 13

--- HomeworkSeminar2/Task13/Program.cs
+++ HomeworkSeminar2/Task13/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет. Не использовать массив
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Enter num : ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+
+if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+{while (num > 999)
+{
+num /= 10;
+}
+num = num % 10;
+Console.WriteLine($"{num}");}
