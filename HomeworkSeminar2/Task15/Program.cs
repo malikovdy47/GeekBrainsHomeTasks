9fcfc4c97299d220d9e69bf5974e28c66eb93c4b
Denis Malikov <malikovdy47@gmail.com>
2023-08-05 18:44:11 +0300

    Решена задача 15

--- HomeworkSeminar2/Task15/Program.cs
+++ HomeworkSeminar2/Task15/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день 
+// недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine("Enter num : ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+
+if (num <= 0)
+{
+    Console.WriteLine($"{num} - это не день недели");
+}
+else if (num >= 8)
+{
+    Console.WriteLine($"{num} - это не день недели");
+}
+else if (num >= 6 && num < 8)
+{
+    Console.WriteLine($"{num} - это выходной");
+}
+else if (num < 6)
+{
+    Console.WriteLine($"{num} - это будний день");
+}  
