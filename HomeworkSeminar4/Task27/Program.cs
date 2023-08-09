3fc3154dab7cb22c6447c13332c06fc3ff4e8c40
Denis Malikov <malikovdy47@gmail.com>
2023-08-09 23:04:34 +0300

    Решена задача 27 семинара 4

--- HomeworkSeminar4/Task27/Program.cs
+++ HomeworkSeminar4/Task27/Program.cs
@@ -4,3 +4,25 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.WriteLine("Enter num : ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+
+  int SumNum(int num)
+  {
+    
+    int count = Convert.ToString(num).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+      advance = num - num % 10;
+      result = result + (num - advance);
+      num = num / 10;
+    }
+   return result;
+  }
+
+int sumNum = SumNum(num);
+Console.WriteLine("Сумма цифр в числе: " + sumNum);
