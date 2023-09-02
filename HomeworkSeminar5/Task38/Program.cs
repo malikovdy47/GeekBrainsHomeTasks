01d9557f987296a387fcb03f4b7d46aa6745ab2c
Denis Malikov <malikovdy47@gmail.com>
2023-09-02 09:07:50 +0300

    Решена задача 38

---
+++ HomeworkSeminar5/Task38/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+//[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+double[] randomArray = new double[a];
+
+void mas(int a)
+    {
+        Random rand = new Random();
+        for (int i = 0; i < a; i++)
+            {
+                randomArray[i] = rand.NextDouble();
+                Console.WriteLine($"{randomArray[i]:F2} ");
+            }
+
+}
+
+double raz(double[] randomArray)
+    {
+        double min = randomArray[0];
+        double max = randomArray[0];
+        int i = 1;
+        while (i < randomArray.Length)
+            {
+                if (max<randomArray[i])
+                max = randomArray[i];
+                if (min>randomArray[i])
+                min = randomArray[i];
+                i = i + 1;
+            }
+return max-min;
+}
+
+mas(a);
+Console.Write($"Разница между максимальным и минимальным элементами массива: {raz(randomArray):F2}");
