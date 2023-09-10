995bafb44b4417cf8fe3f24bac1dbcb6d32f026d
Denis Malikov <malikovdy47@gmail.com>
2023-09-10 08:55:00 +0300

    Решены задачи семинара

--- HomeworkSeminar8/Task56/Program.cs
+++ HomeworkSeminar8/Task56/Program.cs
@@ -1,13 +1,12 @@
-﻿/*Задача 54: Задайте двумерный массив. Напишите программу, 
-которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить 
+строку с наименьшей суммой элементов.
 Например, задан массив:
 1 4 7 2
 5 9 2 3
 8 4 2 4
-В итоге получается вот такой массив:
-7 4 2 1
-9 5 3 2
-8 4 4 2*/
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки 
+с наименьшей суммой элементов: 1 строка*/
 
 internal class Program
 {
@@ -21,27 +20,29 @@ internal class Program
         CreateArray(array);
         WriteArray(array);
 
-        Console.WriteLine($"\nСортированный массив: ");
-        OrderArrayLines(array);
-        WriteArray(array);
-
-        void OrderArrayLines(int[,] array)
+        int minSumLine = 0;
+        int sumLine = SumLineElements(array, 0);
+        for (int i = 1; i < array.GetLength(0); i++)
         {
-            for (int i = 0; i < array.GetLength(0); i++)
+            int tempSumLine = SumLineElements(array, i);
+            if (sumLine > tempSumLine)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    for (int k = 0; k < array.GetLength(1) - 1; k++)
-                    {
-                        if (array[i, k] < array[i, k + 1])
-                        {
-                            int temp = array[i, k + 1];
-                            array[i, k + 1] = array[i, k];
-                            array[i, k] = temp;
-                        }
-                    }
-                }
+                sumLine = tempSumLine;
+                minSumLine = i;
             }
+        }
+
+        Console.WriteLine($"\n{minSumLine + 1} - строкa с наименьшей суммой элементов = {sumLine}");
+
+
+        int SumLineElements(int[,] array, int i)
+        {
+            int sumLine = array[i, 0];
+            for (int j = 1; j < array.GetLength(1); j++)
+            {
+                sumLine += array[i, j];
+            }
+            return sumLine;
         }
 
         int InputNumbers(string input)
